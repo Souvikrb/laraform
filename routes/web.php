6c8c28e7a00e05cf738f6e9e27ca8e7e16147262
souvikrb <souvikraybarman@gmail.com>
2022-02-24 20:24:22 +0530

    form component create

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\http\Controller\ContactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[ContactController::class,'index']);
 
-Route::get('/home', function () {
-    return view('welcome');
-});
+
